b5f8948b911303508ee02799d8ccccadbb615518
Taylor Coffman <CoffmanTaylor96+git@gmail.com>
2021-02-23 21:54:01 -0800

    Got the basics of the framework working

---
+++ src/lib.rs
@@ -0,0 +1,177 @@
+use indexmap::set::IndexSet;
+use std::{collections::VecDeque, hash::Hash};
+
+pub trait SearchState: Sized {
+    fn get_transitions(&self) -> IndexSet<Self>;
+}
+
+pub struct BreathFirstTester<S> {
+    to_search: VecDeque<S>,
+    already_searched: IndexSet<S>,
+    invariants: Vec<fn(&S) -> bool>,
+    end_condition: fn(&S) -> bool,
+}
+
+impl<S> BreathFirstTester<S> {
+    pub fn new(
+        start: S,
+        invariants: Vec<fn(&S) -> bool>,
+        end_condition: fn(&S) -> bool,
+    ) -> BreathFirstTester<S>
+    where
+        S: Eq + Hash,
+    {
+        let mut to_search = VecDeque::new();
+        to_search.push_back(start);
+        BreathFirstTester {
+            to_search,
+            already_searched: IndexSet::new(),
+            invariants,
+            end_condition,
+        }
+    }
+
+    pub fn search(&mut self) -> Result<S, S>
+    where
+        S: Eq + Hash + SearchState,
+    {
+        while let Some(state) = self.to_search.pop_front() {
+            // Check the invariants.
+            if !self.invariants.iter().all(|inv| inv(&state)) {
+                return Err(state);
+            }
+
+            // Check the end condition.
+            if (self.end_condition)(&state) {
+                return Ok(state);
+            }
+
+            // Add all of the states after each transition to the search list if they are not already known.
+            for next_state in state.get_transitions().drain(..) {
+                if !self.to_search.contains(&next_state)
+                    && !self.already_searched.contains(&next_state)
+                    && next_state != state
+                {
+                    self.to_search.push_back(next_state);
+                }
+            }
+
+            self.already_searched.insert(state);
+        }
+
+        unimplemented!("Exhaustive searches are not implemented yet.")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn first_state_breaks() {
+        #[derive(Hash, Eq, PartialEq, Debug)]
+        struct State();
+
+        impl SearchState for State {
+            fn get_transitions(&self) -> IndexSet<Self> {
+                IndexSet::new()
+            }
+        }
+
+        let mut tester = BreathFirstTester::new(State(), vec![|_s| false], |_s| false);
+
+        assert_eq!(Err(State()), tester.search());
+    }
+
+    #[test]
+    fn first_state_ends() {
+        #[derive(Hash, Eq, PartialEq, Debug)]
+        struct State();
+
+        impl SearchState for State {
+            fn get_transitions(&self) -> IndexSet<Self> {
+                IndexSet::new()
+            }
+        }
+
+        let mut tester = BreathFirstTester::new(State(), Vec::new(), |_s| true);
+
+        assert_eq!(Ok(State()), tester.search());
+    }
+
+    #[test]
+    fn chain_to_end() {
+        #[derive(Hash, Eq, PartialEq, Debug)]
+        struct State(usize);
+
+        impl SearchState for State {
+            fn get_transitions(&self) -> IndexSet<Self> {
+                let mut out = IndexSet::new();
+                out.insert(State(self.0 + 1));
+                out
+            }
+        }
+
+        let mut tester = BreathFirstTester::new(State(0), Vec::new(), |s| s == &State(10));
+
+        assert_eq!(Ok(State(10)), tester.search());
+    }
+
+    #[test]
+    fn chain_to_brake() {
+        #[derive(Hash, Eq, PartialEq, Debug)]
+        struct State(usize);
+
+        impl SearchState for State {
+            fn get_transitions(&self) -> IndexSet<Self> {
+                let mut out = IndexSet::new();
+                out.insert(State(self.0 + 1));
+                out
+            }
+        }
+
+        let mut tester = BreathFirstTester::new(State(0), vec![|s| s.0 != 10], |_s| false);
+
+        assert_eq!(Err(State(10)), tester.search());
+    }
+
+    #[test]
+    fn tree_to_end() {
+        #[derive(Hash, Eq, PartialEq, Debug)]
+        struct State(usize, usize);
+
+        impl SearchState for State {
+            fn get_transitions(&self) -> IndexSet<Self> {
+                let mut out = IndexSet::new();
+                out.insert(State(self.0 + 1, self.1));
+                out.insert(State(self.0, self.1 + 1));
+                out
+            }
+        }
+
+        let mut tester = BreathFirstTester::new(State(0, 0), Vec::new(), |s| s == &State(10, 5));
+
+        assert_eq!(Ok(State(10, 5)), tester.search());
+    }
+
+    #[test]
+    fn tree_to_break() {
+        #[derive(Hash, Eq, PartialEq, Debug)]
+        struct State(usize, usize);
+
+        impl SearchState for State {
+            fn get_transitions(&self) -> IndexSet<Self> {
+                let mut out = IndexSet::new();
+                out.insert(State(self.0 + 1, self.1));
+                out.insert(State(self.0, self.1 + 1));
+                out
+            }
+        }
+
+        let mut tester = BreathFirstTester::new(State(0, 0), vec![|s| s != &State(10, 5)], |s| {
+            s.0 > 20 || s.1 > 20
+        });
+
+        assert_eq!(Err(State(10, 5)), tester.search());
+    }
+}
