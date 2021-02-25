25234bfa163b48769e91ab826ee63d5924fa052f
Taylor Coffman <CoffmanTaylor96+git@gmail.com>
2021-02-24 19:46:15 -0800

    Added documentation

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,14 @@ use std::{
     hash::Hash,
 };
 
+/// This trait means that the struct this is implemented on can be used to define a specific state
+/// of the System. `get_transitions` must be deterministic and idempotent. It is a logical error if
+/// two equal `SearchState`s return different results from `get_transitions`.
 pub trait SearchState: Sized {
     fn get_transitions(&self) -> HashSet<Self>;
 }
 
+/// Defines the System that you want to search. The System contains at least one start state.
 pub struct SearchConfig<'a, S> {
     start_states: VecDeque<S>,
     invariants: Vec<(fn(&S) -> bool, &'a str)>,
@@ -14,6 +18,7 @@ pub struct SearchConfig<'a, S> {
 }
 
 impl<'a, S> SearchConfig<'a, S> {
+    /// Constructs a new `SearchConfig` using the provided state as the only start state.
     pub fn new(start_state: S) -> SearchConfig<'a, S>
     where
         S: Eq + Hash,
@@ -27,11 +32,15 @@ impl<'a, S> SearchConfig<'a, S> {
         }
     }
 
+    /// Adds an invariant to the System. Every state discovered during the search, including
+    /// the state states, will be checked against every invariant of the System.
     pub fn add_invariant(mut self, name: &'a str, inv: fn(&S) -> bool) -> Self {
         self.invariants.push((inv, name));
         self
     }
 
+    /// Adds a collection of invariants to the System. Every state discovered during the search,
+    /// including the state states, will be checked against every invariant of the System.
     pub fn add_invariants<I>(mut self, invs: I) -> Self
     where
         I: IntoIterator<Item = (fn(&S) -> bool, &'a str)>,
@@ -40,11 +49,19 @@ impl<'a, S> SearchConfig<'a, S> {
         self
     }
 
+    /// Adds a pruning condition to the System. When preforming a search, if a state matches
+    /// any of the pruning conditions, none of that state's transitions will be searched. This
+    /// is helpful for speeding up searches when you know there are states that once you reach,
+    /// you know you wont find the end condition afterwards.
     pub fn add_prune_condition(mut self, prune_condition: fn(&S) -> bool) -> Self {
         self.prune_conditions.push(prune_condition);
         self
     }
 
+    /// Preforms a Breath First Search on the System looking for the a state that matches the given
+    /// end condition. If a matching state is found, it is returned in `Ok(State)`. If any of the
+    /// System's invariants are violated, then the state that failed and the name of the invariant
+    /// are returned as `Err((State, name))`.
     pub fn search_bfs(&self, end_condition: fn(&S) -> bool) -> Result<S, (S, &'a str)>
     where
         S: Eq + Hash + SearchState + Clone,
